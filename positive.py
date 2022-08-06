f47f6c0b8d63216dc8acc7a2be2b424b8931dd8c
armaanSaini10 <110713658+armaanSaini10@users.noreply.github.com>
2022-08-06 16:16:04 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  6 15:55:59 2022
+
+@author: armaa
+"""
+
+#python program to print positive numbers
+
+list1 =[12, -7, 5, 64, -14, 10]
+for num in list1:
+    if num>= 0:
+        print(num, end="  ")
+       
