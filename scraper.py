52639c1eee11b9bb788d2462db9f3be3b73bd832
armaanSaini10 <110713658+armaanSaini10@users.noreply.github.com>
2022-08-07 19:03:42 +0530

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,43 @@
+# Scraping
+import requests
+from bs4 import BeautifulSoup
+import pandas
+
+oyo_url ="https://www.oyorooms.com/hotels-in-bangalore/"
+page_num_MAX = 3
+scraped_info_list = []
+
+for page_num in range(1, page_num_MAX):
+    req= requests.get(oyo_url)
+    content = req.content
+
+    soup = BeautifulSoup(content, "html.parser")
+
+    all_hotels = soup.find_all("div", {"class":"hotelCardListing"})
+  
+    
+for hotel in all_hotels:
+    hotel_dict ={}
+    hotel_dict("name") = hotel.find("h3", {"class": "listingHotelDescription_hotelName"}).text
+    hotel_dict("address") = hotel.find("span", {"itemprop": "streetAddress"}).text
+    hotel_dict("price") = hotel.find("span", {"class": "listingprice__finalPrice" }).text
+    
+    try:
+        hotel_dict("rating") = hotel.find("span", {"class": "hotelRating__ratingSummary"}).text
+    except AttributeError:
+        pass
+    
+    parent_amenities_element = hotel.find("div", {"class": "amenityWrapper"})
+    
+    for amenity in parent_amenities_element.find_all("div", {"class": "amenityWrapper__amenity"}):
+        amenities_list.append(amenity.find("span", {"class": "d-body-sm"}).text.strip())
+
+    hotel_dict["amenities"] = ','.join(amenities_list[:-1])
+    
+    scraped_info_list.append(hotel_dict)
+    
+   # print(hotel_name, hotel_address, hotel_price, hotel_rating)
+ 
+dataFrame = pandas.DataFrame(scraped_info_list)
+dataFrame.to_csv("Oyo.csv")   
+    
